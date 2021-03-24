8be3acb27e2d9a282a7650e016094e2c47f05305
borisMontavon <boris.montavon@gmail.com>
2021-03-24 21:21:25 +0100

    Watch-scss config, header style, first section part of style

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
 
-const srcFolder = './src/scss/*';
+const mainFolder = './src/scss/*.scss';
+const moduleFolder = './src/scss/*/*.scss';
 const destFolder = './src/css/';
 
 gulp.task('styles', () => {
-    return gulp.src(srcFolder)
+    return gulp.src(mainFolder)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(destFolder));
 });
@@ -20,5 +21,6 @@ gulp.task('clean', () => {
 gulp.task('default', gulp.series(['clean', 'styles']));
 
 gulp.task('watch', function () {
-   gulp.watch(srcFolder, gulp.series('styles'));
+    gulp.watch(mainFolder, gulp.series('styles'));
+    gulp.watch(moduleFolder, gulp.series('styles'));
 });
